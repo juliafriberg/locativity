18d3c431d8251bc7a2381373a2b4eca0c2f3750b
Julia Friberg <julia.friberg@varvet.com>
2020-04-03 22:07:51 +0200

    Initial Commit

---
+++ ShareLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareLocation
+//
+//  Created by Julia Friberg on 2020-04-03.
+//  Copyright © 2020 Julia Friberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShareLocationTests/ShareLocationTests.swift
@@ -0,0 +1,34 @@
+//
+//  ShareLocationTests.swift
+//  ShareLocationTests
+//
+//  Created by Julia Friberg on 2020-04-03.
+//  Copyright © 2020 Julia Friberg. All rights reserved.
+//
+
+import XCTest
+@testable import ShareLocation
+
+class ShareLocationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
